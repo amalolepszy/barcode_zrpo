144530cd2b8a2e3878b201de1431d875795d33d0
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-01-24 22:27:48 +0100

    Utworzenie repo z plikiem

---
+++ barcode_scanner.m
@@ -0,0 +1,25 @@
+
+%Wyczyszczenie obszaru roboczego oraz konsoli
+close all;
+clc;
+
+%wczytanie obrazu do pamięci podręcznej
+a = imread('barcode.jpg');
+imshow(a);
+
+%użycie funkcji w celu przypisania wartości i wyświetlenie w konsoli
+%msg - wiadomość przypisana do kodu paskowego
+%detectedBarcode - jest to rodzaj wykrytego barcode'u
+%loc - zmienna odpowiadająca za obszar na którym wykryty został kod
+[msg,detectedBarcode, loc] = readBarcode(a);
+disp("Decoded barcode message: " + msg);
+disp("barcode type: " + detectedBarcode);
+
+
+% Zlokalizowanie początku i umieszczenie linii w miejscu kodu paskowego
+% Wyświetlenie obrazu
+xyBegin = loc(1,:);
+Imsg = insertText(a,xyBegin,msg,'BoxOpacity',1,'FontSize',30);
+imSize = size(Imsg);
+Imsg = insertShape(Imsg,'Line',[1 xyBegin(2) imSize(2) xyBegin(2)],'LineWidth',5);
+imshow(Imsg);
