d943d174ceeb2f7dbb03198b5fba37463d62d815
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-01-30 19:23:11 +0100

    Ogarnięcie kodów 2-D

--- barcode_scanner.m
+++ barcode_scanner.m
@@ -7,7 +7,6 @@ clc;
 %wczytywanie barcode'ów 1D
 %----------------------------------------------------
 
-%wczytanie obrazu do pamięci podręcznej
 upca = imread('upc-a.jpg');
 upce = imread('upc-e.jpg');
 ean8 = imread('ean-8.jpg');
@@ -71,6 +70,51 @@ end
 %wczytywanie kodów 2-D
 %------------------------------------------------------------------------
 
+qr = imread('qr-code.jpg');
+datamatrix = imread('datamatrix.jpg');
+aztec = imread('aztec.jpg');
+pdf = imread('pdf417.jpg');
+
+%Deklaracja cell-arrayów zawierających zmienne barcode'ow oraz tytuły w
+%celu łatwiejszej iteracji
+tab_2D = {qr, datamatrix, aztec, pdf};
+title_2D = {'QR-CODE', 'DATA-MATRIX', 'AZTEC', 'PDF-417'};
+
+%Wyświetlenie nieprzetworzonych barcodów 2-D
+figure('Name', 'Kody 2-D');
+
+i = 1;
+while i <= size(tab_2D,2)
+    subplot(2,2,i);
+    imshow(cell2mat(tab_2D(i))); %zmiana typu cell na array w celu wyświetlenia
+    title(string(title_2D(i))); %zmiana typu cell na string
+    i=i+1;
+end
+
+i = 1;
+while i <= size(tab_2D,2)
+    a = cell2mat(tab_2D(i)); %zmiana typu cell na array
+    
+    %użycie funkcji w celu przypisania wartości i wyświetlenie w konsoli
+    %msg - wiadomość przypisana do kodu paskowego
+    %detectedBarcode - jest to rodzaj wykrytego barcode'u
+    %loc - zmienna odpowiadająca za obszar na którym wykryty został kod
+    [msg,detectedBarcode, loc] = readBarcode(a);
+    disp("Decoded barcode message: " + msg);
+    disp("barcode type: " + detectedBarcode);
+
+    xyText =  loc(2,:);
+    Imsg = insertText(a,xyText,msg,"BoxOpacity",1,"FontSize",25);
+
+    Imsg = insertShape(Imsg, "FilledCircle", [loc, ...
+        repmat(10, length(loc), 1)],"Color","red","Opacity",1);
+
+    % Wyświetlenie przetworzonego obrazu
+    subplot(2,2,i);
+    imshow(Imsg);
+    title('Wykryte: '+detectedBarcode);
+    xlabel('Odkodowane: '+msg);
 
 
-
+    i=i+1;
+end
